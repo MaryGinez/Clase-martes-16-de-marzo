82095c8f6c2830315f2c1bd234a0fb49883597de
MaryGinez <80795368+MaryGinez@users.noreply.github.com>
2021-04-06 01:12:50 -0500

    Add files via upload

---
+++ Código 2 (Viernes 19). Py.py
@@ -0,0 +1,7 @@
+print("Calcularemos el área de un paralelogramo con los datos indicados")
+base=int(input("Ingresa la base: "))
+altura=int(input("Ingresa la altura: "))
+
+area=base*altura
+
+print(area)
