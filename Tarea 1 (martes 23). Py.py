ae076d745d562af95d662873af481a82bd3120de
MaryGinez <80795368+MaryGinez@users.noreply.github.com>
2021-04-06 01:14:38 -0500

    Add files via upload

---
+++ Tarea 1 (martes 23). Py.py
@@ -0,0 +1,11 @@
+#PROMEDIO DE 5 CALIFICACIONES 
+print("PROMEDIO DEL SEMESTRE")
+#INGRESA CALIFICACIONES
+Calif1 = float(input("CALIFICACIÓN  1 MATEMÁTICAS: "))
+Calif2 = float(input("CALIFICACIÓN  2 ESPAÑOL: "))
+Calif3 = float(input("CALIFICACIÓN  3 HISTORIA: "))
+Calif4 = float(input("CALIFICACIÓN  4 QUIMICA: "))
+Calif5 = float(input("CALIFICACIÓN  5 FÍSICA: "))
+#CALCULAR PROMEDIO
+PROMEDIO= float(Calif1+Calif2+Calif3+Calif4+Calif5)/5
+print("El PROMEDIO ES : ", PROMEDIO)
